ba258fbf70f7c89ffa7dcb7fdbd5890a5367b3b1
sivaprakash0 <31100688+sivaprakash0@users.noreply.github.com>
2018-07-13 12:35:30 +0530

    Update pal.java

--- pal.java
+++ pal.java
@@ -1,7 +1,7 @@
 public class pal {
 public static void main(String[] args) {
 	int no=Integer.parseInt(args[0]);
-	if(no>=1000){
+	if(no<=1000){
    String re=Integer.toString(no);
    String reor=new StringBuffer(re).reverse().toString(); 
    if(re.equals(reor)){
