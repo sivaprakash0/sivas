5367a8141e990e9ce8da712b98c08a28783fd752
sivaprakash0 <31100688+sivaprakash0@users.noreply.github.com>
2018-07-13 14:43:22 +0530

    Update multno.java

--- multno.java
+++ multno.java
@@ -1,7 +1,7 @@
 public class mulno {
 public static void main(String[] args) {
 	int no=Integer.parseInt(args[0]);
-	for(int i=1;i<=no;i++){
+	for(int i=1;i<=5;i++){
 		System.out.println(i*no);
 	}
 }
