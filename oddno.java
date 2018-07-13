74ec45ceb3c5e78b8c4aec6f1fac9c825eb102d2
sivaprakash0 <31100688+sivaprakash0@users.noreply.github.com>
2018-07-13 14:03:21 +0530

    Add files via upload

---
+++ oddno.java
@@ -0,0 +1,11 @@
+public class oddno {
+public static void main(String[] args) {
+	int a=Integer.parseInt(args[0]);
+	int b=Integer.parseInt(args[1]);
+	for(int i=a+1;i<b;i++){
+		if(i%2!=0){
+			System.out.println(i);
+		}
+	}
+}
+}
