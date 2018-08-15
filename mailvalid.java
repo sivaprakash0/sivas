c3c6392878cc8448b6a77ca7c08b64aaedd28bd1
sivaprakash0 <31100688+sivaprakash0@users.noreply.github.com>
2018-08-15 11:42:29 +0530

    Update mailvalid.java

--- mailvalid.java
+++ mailvalid.java
@@ -3,12 +3,37 @@ public class mailvalid {
 public static void main(String[] args) {
 	Scanner in=new Scanner(System.in);
 	String s=in.next();
-	if(s.substring(s.length()-4,s.length()).equals(".com")){
+	int cn=0;
+	for(int i=0;i<s.length();i++){
+		char c[]=s.toCharArray();
+		if(c[i]=='@'){
+			cn++;
+		}
+	}
+	if(cn==1 && s.substring(s.length()-4,s.length()).equals(".com")){
 		int ind=s.indexOf('@');
 		int len=(s.substring(0,ind)).length();
 		int lslen=(s.substring(ind,s.length()-4)).length();
 		if(len>=3 && lslen>=4){
+			int cnt=0,cnt1=0;
+			for(int i=0;i<len;i++){
+				char c1[]=(s.substring(0,ind)).toCharArray();
+				if(c1[i]!='.'){
+					cnt++;
+				}
+			}
+			for(int i=0;i<lslen;i++){
+				char c2[]=(s.substring(ind,s.length()-4)).toCharArray();
+				if(c2[i]!='.'){
+					cnt1++;
+				}
+			}
+			if(cnt==len && cnt1==lslen){
 			System.out.println("YES");
+		   }
+			else{
+				System.out.println("NO");
+			}
 		}
 		else{
 			System.out.println("NO");
