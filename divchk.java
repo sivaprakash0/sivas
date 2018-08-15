403d501fad6269923a9f1932a1109b5f20307a7a
sivaprakash0 <31100688+sivaprakash0@users.noreply.github.com>
2018-08-15 10:26:25 +0530

    Update divchk.java

--- divchk.java
+++ divchk.java
@@ -1,7 +1,4 @@
-package set4;
-
 import java.util.Scanner;
-
 public class divchk {
 public static void main(String[] args) {
 	Scanner in=new Scanner(System.in);
@@ -17,8 +14,6 @@ public static void main(String[] args) {
 	}
 	else{
 		System.out.println("no");
-	}
-	
+	}	
 }
 }
-
