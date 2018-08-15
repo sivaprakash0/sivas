f82b2b740a8945628fdad5bda15b493491ae63f8
sivaprakash0 <31100688+sivaprakash0@users.noreply.github.com>
2018-08-15 09:44:38 +0530

    Add files via upload

---
+++ arrsrtch.java
@@ -0,0 +1,26 @@
+package set4;
+
+import java.util.Scanner;
+
+public class arrsrtch {
+public static void main(String[] args) {
+	Scanner in=new Scanner(System.in);
+	int sz=in.nextInt();
+	int arr[]=new int[sz];
+	int cnt=0;
+	for(int i=0;i<sz;i++){
+		arr[i]=in.nextInt();
+	}
+	for(int j=0;j<sz-1;j++){
+		if(arr[j]<arr[j+1]){
+			cnt++;
+		}
+	}
+	if(cnt==arr.length-1){
+		System.out.println("yes");
+	}
+	else{
+		System.out.println("no");
+	}
+}
+}
